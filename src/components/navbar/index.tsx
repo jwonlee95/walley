dd30e3546adb698c7bf8fe9ec38ec67211df4101
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-01-05 21:48:10 -0500

    Update Setting Environment

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "styles/index.scss";
+import Logo from "assets/logo.png";
+import { Stack, Button } from "@mui/material";
 export const NavBar: React.FC<{}> = () => {
   return (
-    <>
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
-      <Link to="/signup">
-        <button>Sign Up</button>
-      </Link>
-      <Link to="/about">
-        <button>About Us</button>
-      </Link>
-    </>
+    <nav>
+      <div className="logo">
+        <img src={Logo} alt="logo" />
+      </div>
+      <Stack className="stack" spacing={2} direction="row">
+        <Link to="/about">
+          <Button variant="contained">About Us</Button>
+        </Link>
+        <Link to="/login">
+          <Button variant="contained">Login</Button>
+        </Link>
+        <Link to="/signup">
+          <Button variant="contained">Sign Up</Button>
+        </Link>
+      </Stack>
+    </nav>
   );
 };
