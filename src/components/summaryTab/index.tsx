125fa36eed9fb6df99b87043a502dfc32fda6bbd
Jaewon Lee <jwonlee95@gmail.com>
2022-02-01 04:35:12 +0900

    Update Charts

--- src/components/summaryTab/index.tsx
+++ src/components/summaryTab/index.tsx
@@ -16,6 +16,7 @@ import { StateContext, UserContext } from "contexts";
 import { ITransaction } from "interfaces";
 import { reducerState } from "common/store";
 import moment from "moment";
+import { daysToWeeks } from "date-fns";
 
 const TabSectionHeading: React.FC<{ title: string }> = (props) => {
   return <div className="tab-section-heading">{props.title}</div>;
@@ -43,45 +44,23 @@ const barOptions = {
   },
 };
 
-// const barData = {
-//   labels: ["January", "February", "March", "April", "May", "June", "July"],
-//   datasets: [
-//     {
-//       label: "Budget",
-//       data: [1, 2, 3, 4, 5, 6, 7],
-//       backgroundColor: "rgba(255, 99, 132, 0.5)",
-//     },
-//     {
-//       label: "Spent",
-//       data: [7, 6, 5, 4, 3, 2, 1],
-//       backgroundColor: "rgba(53, 162, 235, 0.5)",
-//     },
-//   ],
-// };
-
-interface IParseData {
-  value: number;
-}
-
 const BarChartSection = () => {
-  const dispatch = useDispatch();
-  const { user } = useContext(UserContext).userState;
   const { transaction } = useContext(StateContext);
-  const userSelector = useSelector((state: reducerState) => state.user);
 
-  const now = Date.now();
-  const nowYear = new Date(now).getFullYear(); //2022
-  const nowMonth = new Date(now).getMonth(); //1월 = 0, 2월 = 1....
-
-  const nowDate = new Date(now).getUTCDate(); //지금 날짜
-  const nowDay = moment(now).day(1).day();
-  console.log("Now is", nowDay);
-  const monthData: number[] = [];
-
-  var temp = 0;
   const sortedTransaction = transaction.sort((x, y) =>
     y.date.toLocaleString().localeCompare(x.date.toLocaleString())
   );
+
+  var tempMonth = 0;
+
+  const now = moment().format("MM-DD-YYYY");
+  const nowYear = moment().year();
+  const nowMonth = moment().month();
+  const nowDay = moment().day(); //화요일: 2
+  const nowDate = moment().date(); //1일
+
+  //console.log("Now is", nowDate);
+  const monthData: number[] = [];
 
   for (var i = 0, max = sortedTransaction.length; i < max; i++) {
     if (sortedTransaction[i].type === "expense") {
@@ -89,8 +68,8 @@ const BarChartSection = () => {
         const ele = sortedTransaction[i];
         var dt = new Date(ele.date);
         const month = dt.getMonth();
-        monthData[month] = temp + ele.amount;
-        temp = monthData[month];
+        monthData[month] = tempMonth + ele.amount;
+        tempMonth = monthData[month];
       } else {
         const ele = sortedTransaction[i];
         const previous = sortedTransaction[i - 1];
@@ -99,18 +78,18 @@ const BarChartSection = () => {
         const previousMonth = pdt.getMonth();
         const month = dt.getMonth();
         if (month === previousMonth) {
-          monthData[month] = temp + ele.amount;
-          temp = monthData[month];
+          monthData[month] = tempMonth + ele.amount;
+          tempMonth = monthData[month];
         } else {
-          temp = 0;
-          monthData[month] = temp + ele.amount;
-          temp = monthData[month];
+          tempMonth = 0;
+          monthData[month] = tempMonth + ele.amount;
+          tempMonth = monthData[month];
         }
       }
     }
   }
 
-  const barData = {
+  const barDataMonth = {
     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug"],
     datasets: [
       {
@@ -125,10 +104,177 @@ const BarChartSection = () => {
       },
     ],
   };
-  console.log("Not UseEffect", monthData);
+
   return (
     <div style={{ width: 1523 }}>
-      <Bar options={barOptions} data={barData} />
+      <Bar options={barOptions} data={barDataMonth} />
+    </div>
+  );
+};
+
+const BarChartSectionWeek = () => {
+  const { transaction } = useContext(StateContext);
+
+  const sortedTransaction = transaction.sort((x, y) =>
+    y.date.toLocaleString().localeCompare(x.date.toLocaleString())
+  );
+
+  var tempWeek = 0;
+
+  const weekData: number[] = [];
+
+  const weekTest = () => {
+    let mondayOfWeek = moment().year(2022).month(0).date(1).day(8);
+    if (mondayOfWeek.date() > 7) mondayOfWeek.day(-6);
+
+    let nextMondayOfWeek = mondayOfWeek.clone().add(7, "day");
+    //console.log(mondayOfWeek.format("l"));
+    //console.log(nextMondayOfWeek.format("l"));
+
+    let count = 0;
+    const weeklyRange: any[] = [];
+    while (moment(mondayOfWeek).year() === moment(nextMondayOfWeek).year()) {
+      const weekRange = {
+        startDate: moment(mondayOfWeek).format("l"),
+        endDate: moment(nextMondayOfWeek).subtract(1, "day").format("l"),
+      };
+      weeklyRange.push(weekRange);
+      //console.log(count);
+      mondayOfWeek.add(7, "days");
+      nextMondayOfWeek.add(7, "day");
+    }
+    let weekNumber = 0;
+    for (var i = 0, max = weeklyRange.length; i < max; i++) {
+      for (var j = 0, maxT = sortedTransaction.length; j < maxT; j++) {
+        if (sortedTransaction[j].type === "expense") {
+          const ele = sortedTransaction[j];
+          var dt = moment(ele.date).format("MM-DD-YYYY");
+          if (
+            moment(dt).isBetween(
+              weeklyRange[i].startDate,
+              weeklyRange[i].endDate,
+              undefined,
+              "[]"
+            )
+          ) {
+            weekNumber = i;
+            weekData[i] = tempWeek + ele.amount;
+            tempWeek = weekData[i];
+          }
+        }
+      }
+    }
+    console.log("Week Number is: ", weekData);
+    //console.log(weeklyRange);
+  };
+
+  weekTest();
+
+  const barDataWeek = {
+    labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    datasets: [
+      {
+        label: "Weekly Spent",
+        data: weekData,
+        backgroundColor: "red",
+      },
+    ],
+  };
+  return (
+    <div style={{ width: 1523 }}>
+      <Bar options={barOptions} data={barDataWeek} />
+    </div>
+  );
+};
+
+const BarChartSectionDay = () => {
+  const { transaction } = useContext(StateContext);
+
+  const sortedTransaction = transaction.sort((x, y) =>
+    y.date.toLocaleString().localeCompare(x.date.toLocaleString())
+  );
+
+  const now = moment().format("MM-DD-YYYY");
+  const nowq = moment().format("ll");
+  const nowYear = moment().year();
+  const nowMonth = moment().month();
+  const yesterday = moment().subtract(1, "days").format("MM-DD-YYYY"); //화요일: 2
+  const twoDaysBefore = moment().subtract(2, "days").format("MM-DD-YYYY");
+  const threeDaysBefore = moment().subtract(3, "days").format("MM-DD-YYYY");
+  const fourDaysBefore = moment().subtract(4, "days").format("MM-DD-YYYY");
+  const fiveDaysBefore = moment().subtract(5, "days").format("MM-DD-YYYY");
+  const sixDaysBefore = moment().subtract(6, "days").format("MM-DD-YYYY");
+
+  const nowDate = moment().date(); //1일
+
+  console.log("now is ", nowq);
+
+  const dayData: number[] = [];
+
+  let tempDayOne = 0;
+  let tempDayTwo = 0;
+  let tempDayThree = 0;
+  let tempDayFour = 0;
+  let tempDayFive = 0;
+  let tempDaySix = 0;
+  let tempDaySeven = 0;
+
+  const getDayData = () => {
+    for (var i = 0, max = sortedTransaction.length; i < max; i++) {
+      const ele = sortedTransaction[i];
+      console.log(ele);
+      var dt = moment(ele.date).format("MM-DD-YYYY");
+      console.log("dt is: ", dt);
+      if (moment(dt).isSame(now)) {
+        console.log("Today added");
+        dayData[6] = tempDayOne + ele.amount;
+        tempDayOne = dayData[6];
+      } else if (moment(dt).isSame(yesterday)) {
+        dayData[5] = tempDayTwo + ele.amount;
+        tempDayTwo = dayData[5];
+      } else if (moment(dt).isSame(twoDaysBefore)) {
+        dayData[4] = tempDayThree + ele.amount;
+        tempDayThree = dayData[4];
+      } else if (moment(dt).isSame(threeDaysBefore)) {
+        dayData[3] = tempDayFour + ele.amount;
+        tempDayFour = dayData[3];
+      } else if (moment(dt).isSame(fourDaysBefore)) {
+        dayData[2] = tempDayFive + ele.amount;
+        tempDayFive = dayData[2];
+      } else if (moment(dt).isSame(fiveDaysBefore)) {
+        dayData[1] = tempDaySix + ele.amount;
+        tempDaySix = dayData[1];
+      } else if (moment(dt).isSame(sixDaysBefore)) {
+        dayData[0] = tempDaySeven + ele.amount;
+        tempDaySeven = dayData[0];
+      }
+    }
+    console.log("Day Data is: ", dayData);
+  };
+
+  getDayData();
+
+  const barDataDay = {
+    labels: [
+      "6DayBefore",
+      "5DayBefore",
+      "4DayBefore",
+      "3DayBefore",
+      "2DayBefore",
+      "1DayBefore",
+      "Today",
+    ],
+    datasets: [
+      {
+        label: "Daily Spent",
+        data: dayData,
+        backgroundColor: "Blue",
+      },
+    ],
+  };
+  return (
+    <div style={{ width: 1523 }}>
+      <Bar options={barOptions} data={barDataDay} />
     </div>
   );
 };
@@ -200,6 +346,8 @@ export const SummaryTab: React.FC<TabPanelProps> = ({ value, index }) => {
     <>
       {value === index && (
         <>
+          <BarChartSectionDay />
+          <BarChartSectionWeek />
           <BarChartSection />
           <PieChartSection />
         </>
